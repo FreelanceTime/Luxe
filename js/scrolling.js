8f3b9a598877e7b213aa2938f82c1eae0a5bde8b
Anna Romonosova <119095421+FreelanceTime@users.noreply.github.com>
2023-01-21 15:05:37 +0700

    Delete js directory

--- js/scrolling.js
+++
@@ -1,8 +0,0 @@
-$('.js-scroll-trigger').click(function() {
-    let scrollName = $(this).attr('data-scroll'),
-        scrollElem = $(scrollName),
-        scrollTop = scrollElem.offset().top-80;
-    $('html, body').animate({
-    scrollTop: scrollTop
-    }, 500);
-});
